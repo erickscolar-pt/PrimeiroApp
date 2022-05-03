2d471428c022697de28922496d97e4cfa8a11592
Erick Scolar <erick.scolar@outlook.com>
2022-05-03 15:18:23 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as AuthSession from 'expo-auth-session';
+
+import { Button } from '../../components/Button';
+import { SignInContent } from '../../components/SignInContent';
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
+
+
+import { styles } from './styles';
+
+type AuthResponse = {
+  type: string;
+  params: {
+    access_token: string;
+  }
+}
+
+export function SignIn() {
+  const navigation = useNavigation();
+
+  async function handleSignIn() {
+
+
+    const RESPONSE_YPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_YPE}&scope=${SCOPE}`;
+
+    const { type, params } = await AuthSession
+    .startAsync({ authUrl }) as AuthResponse;
+    
+
+    if(type === 'success'){
+      navigation.navigate('Profile', { token: params.access_token });
+    }
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <SignInContent />
+
+      <Button
+        title="Entrar com Google"
+        icon="social-google"
+        onPress={handleSignIn}
+      />
+    </View>
+  );
+}
